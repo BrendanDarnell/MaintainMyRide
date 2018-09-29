b7b01e5bc7193c7ebc7a3b470148c27ff2d6655f
Brendan Darnell <brendan_darnell@hotmail.com>
2018-09-29 14:01:13 -0600

    Add user, vehicle, and maintenance schemas to models.js

--- models.js
+++ models.js
@@ -2,9 +2,27 @@
 
 const mongoose = require("mongoose");
 
-const usersSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
 	firstName: {type: String, required: true},
 	lastName: {type: String, required: true},
 	userName: {type: String, required: true},
 	password: {type: String, required: true},
+	vehicles: [vehicleSchema]
+})
+
+const vehicleSchema = mongoose.Schema({
+	make: {type: String, required: true},
+	model: {type: String, required: true}, 
+	year: {type: Date, required: true},
+	name: {type: String default: this.make},
+	engine: {type: String}
+})
+
+const maintenanceSchema = mongoose.Schema({
+	vehicleId: {type: mongoose.Schema.Types.ObjectId, ref: 'Vehicle'},
+	type: {type: String, required: true},
+	mileage: {type: String, required: true},
+	nextScheduled: {type: String},
+	notes: {type: String},
+	links: {type: String}
 })
